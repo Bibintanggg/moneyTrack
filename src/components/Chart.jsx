4596240e977df4974cecf847c2a2ac6f760690c3
Bibintanggg <bintangyudhaa8@gmail.com>
2025-01-31 21:30:18 +0700

    Change Chart

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,56 +1,77 @@
-import { ResponsiveContainer, LineChart, Tooltip, YAxis, XAxis, Line, CartesianGrid } from 'recharts';
-import { CustomTooltip, data } from "@tailus-ui/visualizations";
-import { Text, Title } from '@components/typography';
-import Badge from '@react-ui/badge/Badge';
-import { TrendingDown, TrendingUp } from 'lucide-react';
+import React, { PureComponent } from 'react';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ReferenceLine,
+  ResponsiveContainer,
+} from 'recharts';
 
-function Chart() {
+const data = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+
+export default class Example extends PureComponent {
+  static demoUrl = 'https://codesandbox.io/p/sandbox/line-chart-width-xaxis-padding-v3w7s9';
+
+  render() {
     return (
-        <div className="w-full">
-            <Title as="h2" size="lg" weight="medium" className="mb-1">
-                Overview
-            </Title>
-            <Text className="mb-0 mt-1" size="sm">
-                Visualize your main activities data
-            </Text>
-            <Title className="mt-2 flex items-center gap-3" as="span" weight="medium" size="xl">
-                56493
-                <Badge intent="success" size="xs" className="flex h-fit items-center gap-1.5">
-                    <TrendingUp className="size-3.5" />
-                    36%
-                </Badge>
-            </Title>
-
-            <div className="mt-8 h-56 w-full sm:h-72" data-shade="900">
-                <ResponsiveContainer width="100%" height="100%">
-                    <LineChart data={data}>
-                        <defs>
-                            <linearGradient className="text-white dark:text-gray-900" id="dotFill" x1="0" y1="0" x2="0" y2="1">
-                                <stop offset="5%" stopColor="currentColor" stopOpacity={1} />
-                                <stop offset="95%" stopColor="currentColor" stopOpacity={1} />
-                            </linearGradient>
-                        </defs>
-                        <YAxis className="text-caption" width={36} fontSize={12} tickLine={false} axisLine={false} />
-                        <XAxis className="text-caption" dataKey="name" fontSize={12} tickLine={false} axisLine={false} />
-                        <Tooltip cursor={{ stroke: 'var(--ui-border-color)', strokeWidth: 1 }} content={<Custom payload={[]} active fancy label={''} />} />
-
-                        <CartesianGrid horizontal={false} stroke="var(--ui-border-color)" strokeDasharray={3} />
-
-                        <Line
-                            stroke="var(--dv-primary)"
-                            dot={{ fill: 'var(--ui-bg)' }}
-                            dataKey="Primary"
-                            activeDot={{
-                                color: 'var(--dv-primary)',
-                                r: 5,
-                                stroke: 'var(--ui-bg)'
-                            }}
-                        />
-                    </LineChart>
-                </ResponsiveContainer>
-            </div>
-        </div>
-    )
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart width={500} height={300} data={data}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" padding={{ left: 30, right: 30 }} />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+        </LineChart>
+      </ResponsiveContainer>
+    );
+  }
 }
-
-export default Chart;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,29 @@
-import Sidebar from "../components/Sidebar"
+import { AreaChart } from "recharts";
+import Chart from "../components/Chart";
+import Sidebar from "../components/Sidebar";
 
 function Dashboard() {
     return (
-        <div>
-            <Sidebar/>
+        <div className="relative min-h-screen">
+            <Sidebar />
+            <div className="ml-56 p-8">
+                <div className="flex justify-between text-left">
+                    <h1 className="text-base font-semibold font-jakarta pt-2">
+                        Dashboard
+                    </h1>
+                    <div className="text-right">
+                        <h2 className="text-base font-semibold font-jakarta ">
+                            Hello, User !
+                        </h2>
+                        <p className="-translate-y-2 font-jakarta">
+                            Role &gt; Admin
+                        </p>
+                    </div>
+                </div>
+                <div className="w-full h-0.5 mx-auto opacity-25 bg-black"></div>
+            </div>
         </div>
-    )
+    );
 }
 
-export default Dashboard
+export default Dashboard;
